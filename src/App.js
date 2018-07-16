ea46b4bbed8da7aa9dcc448a16097f5fef8fc12f
i3wgnit <i3wgnit@gmail.com>
2018-07-16 13:29:18 -0400

    Initialize repo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import CAL from "chainabstractionlayer"
+import liqUI from "liquality-ui"
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Liquality Atomic Swap</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
