7b3cca189288d1668b64783ae7f2a9806b204e66
monokh <mnokhb@gmail.com>
2018-11-19 13:34:47 +0000

    Redesign: button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './Button.css'
+
+class Button extends Component {
+  render () {
+    const classes = ['Button', 'btn', 'Button_wide']
+
+    if (this.props.className) {
+      classes.push(this.props.className)
+    }
+
+    if (this.props.primary) {
+      classes.push('btn-primary')
+    } else if (this.props.secondary) {
+      classes.push('btn-secondary')
+    }
+
+    return <button className={classes.join(' ')} disabled={this.props.disabled} onClick={e => this.props.onClick(e)}>
+      {this.props.icon && <span class='Button_icon'><img src={this.props.icon} /></span>}
+      {this.props.children}
+    </button>
+  }
+}
+
+Button.propTypes = {
+  className: PropTypes.string,
+  primary: PropTypes.bool,
+  secondary: PropTypes.bool,
+  disabled: PropTypes.bool,
+  icon: PropTypes.any,
+  onClick: PropTypes.func
+}
+
+export default Button
