a4e420fce2f1e10b2c96ae6af5c62de578028f0d
monokh <mnokhb@gmail.com>
2018-11-20 17:50:26 +0000

    Redesign: initial progress stepper

--- src/components/SwapProgressStepper/SwapProgressStepper.js
+++ src/components/SwapProgressStepper/SwapProgressStepper.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Stepper, Step, StepLabel } from '@material-ui/core'
+import { Stepper, Step, StepLabel, StepConnector } from '@material-ui/core'
 import { steps, stepData } from './steps'
+
+import CompletedIcon from '../../icons/completed.svg'
+import './SwapProgressStepper.css'
 
 class SwapProgressStepper extends Component {
   render () {
     const activeStep = stepData.findIndex(step => step.id === this.props.state)
-    return <Stepper activeStep={activeStep}>
+    const connector = (
+      <StepConnector
+        classes={{
+          line: 'SwapProgressStepper_line'
+        }}
+      />)
+    return <Stepper connector={connector} activeStep={activeStep} alternativeLabel className='SwapProgressStepper'>
       {stepData.map((step, index) => {
+        const stepIconProps = {
+          classes: {
+            root: 'SwapProgressStepper_icon',
+            text: 'SwapProgressStepper_icon_text',
+            active: 'SwapProgressStepper_icon_active',
+            completed: 'SwapProgressStepper_icon_completed'
+          }
+        }
+        if (index < activeStep) {
+          stepIconProps.icon = <img src={CompletedIcon} />
+        }
         return <Step key={step.id}>
-          <StepLabel>{step.label}</StepLabel>
+          <StepLabel classes={{
+            label: 'SwapProgressStepper_label',
+            alternativeLabel: 'SwapProgressStepper_alternativeLabel',
+            active: 'SwapProgressStepper_label_active'
+          }} StepIconProps={stepIconProps}>{step.label}</StepLabel>
         </Step>
       })}
     </Stepper>
