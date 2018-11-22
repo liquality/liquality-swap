75303d5b26aefc1b569ede6649cbb11caa2ec424
Harsh Vakharia <harshjv@gmail.com>
2018-11-22 13:36:57 +0530

    Use config params form window & localstorage

--- src/services/chainClient.js
+++ src/services/chainClient.js
@@ -1,14 +1,20 @@
-/* global web3 */
+/* global web3, localStorage */
 
 import { Client, providers, networks } from '@liquality/chainabstractionlayer/dist/index.umd.js'
 
 const ethClient = new Client()
-ethClient.addProvider(new providers.ethereum.EthereumRPCProvider('https://localhost:8545'))
+ethClient.addProvider(new providers.ethereum.EthereumRPCProvider(
+  localStorage.ethRpc || window.ethRpc || 'http://localhost:8545'
+))
 ethClient.addProvider(new providers.ethereum.EthereumMetaMaskProvider(web3.currentProvider))
 ethClient.addProvider(new providers.ethereum.EthereumSwapProvider())
 
 const btcClient = new Client()
-btcClient.addProvider(new providers.bitcoin.BitcoinRPCProvider('https://localhost:18332', 'bitcoin', 'local321'))
+btcClient.addProvider(new providers.bitcoin.BitcoinRPCProvider(
+  localStorage.btcRpc || window.btcRpc || 'http://localhost:18332',
+  localStorage.btcRpcUser || window.btcRpcUser || 'bitcoin',
+  localStorage.btcRpcPass || window.btcRpcPass || 'local321'
+))
 btcClient.addProvider(new providers.bitcoin.BitcoinLedgerProvider({network: networks.bitcoin_testnet}))
 btcClient.addProvider(new providers.bitcoin.BitcoinSwapProvider({network: networks.bitcoin_testnet}))
 
