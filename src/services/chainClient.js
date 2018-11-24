73deb45728447e21cba4f931080ea4f17662ed24
Harsh Vakharia <harshjv@gmail.com>
2018-11-25 01:40:03 +0530

    Use build env vars with localStorage & window

--- src/services/chainClient.js
+++ src/services/chainClient.js
@@ -4,7 +4,7 @@ import { Client, providers, networks } from '@liquality/chainabstractionlayer/di
 
 const ethClient = new Client()
 ethClient.addProvider(new providers.ethereum.EthereumRPCProvider(
-  localStorage.ethRpc || window.ethRpc || 'http://localhost:8545'
+  localStorage.ethRpc || window.ethRpc || process.env.REACT_APP_ETH_RPC || 'http://localhost:8545'
 ))
 if (typeof web3 !== 'undefined') {
   ethClient.addProvider(new providers.ethereum.EthereumMetaMaskProvider(web3.currentProvider))
@@ -13,9 +13,9 @@ ethClient.addProvider(new providers.ethereum.EthereumSwapProvider())
 
 const btcClient = new Client()
 btcClient.addProvider(new providers.bitcoin.BitcoinRPCProvider(
-  localStorage.btcRpc || window.btcRpc || 'http://localhost:18332',
-  localStorage.btcRpcUser || window.btcRpcUser || 'bitcoin',
-  localStorage.btcRpcPass || window.btcRpcPass || 'local321'
+  localStorage.btcRpc || window.btcRpc || process.env.REACT_APP_BTC_RPC || 'http://localhost:18332',
+  localStorage.btcRpcUser || window.btcRpcUser || process.env.REACT_APP_BTC_RPC_USER || 'bitcoin',
+  localStorage.btcRpcPass || window.btcRpcPass || process.env.REACT_APP_BTC_RPC_PASS || 'local321'
 ))
 btcClient.addProvider(new providers.bitcoin.BitcoinLedgerProvider({network: networks.bitcoin_testnet}))
 btcClient.addProvider(new providers.bitcoin.BitcoinSwapProvider({network: networks.bitcoin_testnet}))
