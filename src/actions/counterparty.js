f53a7a924844d9ade7375a883f4678aee69d82ae
Matthew Black <mblackmblackmblack@gmail.com>
2018-12-13 11:28:14 -0500

    Strip 0x for eth

--- src/actions/counterparty.js
+++ src/actions/counterparty.js
@@ -3,6 +3,10 @@ const types = {
 }
 
 function changeCounterPartyAddress (currency, newValue, valid) {
+  if (currency === 'eth') {
+    newValue = newValue.startsWith('0x') ? newValue.slice(2) : newValue
+    newValue = newValue.toLowerCase()
+  }
   return { type: types.CHANGE_COUNTER_PARTY_ADDRESS, currency, newValue, valid }
 }
 
