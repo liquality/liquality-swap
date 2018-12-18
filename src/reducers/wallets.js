4e67182b4d2470b36af9b2cf5fafb4622a2b1c6a
Matthew Black <mblackmblackmblack@gmail.com>
2018-12-18 13:55:17 -0500

    Improve formatting

--- src/reducers/wallets.js
+++ src/reducers/wallets.js
@@ -25,11 +25,6 @@ const initialState = {
 }
 
 function switchSides (state, action) {
-  if (state.b.chosen && !state.b.connected) {
-    state.b.switched = !state.b.switched
-  } else if (state.a.chosen && !state.a.connected) {
-    state.a.switched = !state.a.switched
-  }
   return update(state, {
     a: { $set: state.b },
     b: { $set: state.a }
@@ -55,10 +50,6 @@ function chooseWallet (state, action) {
 }
 
 function connectWallet (state, action) {
-  const otherParty = action.party === 'a' ? 'b' : 'a'
-  if (state[otherParty].switched) {
-    action.party = otherParty
-  }
   return update(state, {
     [action.party]: {
       addresses: { $set: action.addresses },
