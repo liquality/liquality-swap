a572181328e5f460ee178daed3aa7e2d17d23bd3
monokh <mnokhb@gmail.com>
2019-01-23 13:35:02 +0000

    Remove swap challenge

--- src/containers/LiqualitySwap/LiqualitySwap.js
+++ src/containers/LiqualitySwap/LiqualitySwap.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
-import Modal from '@material-ui/core/Modal'
 
 import SwapInitiation from '../SwapInitiation'
 import CounterPartyLinkCard from '../../components/CounterPartyLinkCard/CounterPartyLinkCard'
 import BackupLinkCard from '../../components/BackupLinkCard/BackupLinkCard'
-import Button from '../../components/Button/Button'
 import ErrorModal from '../../components/ErrorModal/ErrorModal'
 import Waiting from '../Waiting'
 import SwapRedemption from '../SwapRedemption'
@@ -56,14 +54,6 @@ class LiqualitySwap extends Component {
         </div>
       </div>
       <footer dangerouslySetInnerHTML={{__html: config.injectFooter}} />
-      {window.location.href.indexOf('challenge') > -1 &&
-      <Modal open>
-        <div className='SwapChallengeModal'>
-          <h4>Thanks to all of you who have participated in the Swap Challenge</h4>
-          <h2>The Challenge is now complete</h2>
-          <Button wide primary onClick={() => window.open('https://liquality.io', '_self')}>Back to home page</Button>
-        </div>
-      </Modal>}
       <ErrorModal open={this.props.error} error={this.props.error} onClose={this.props.clearError} />
     </div>
   }
