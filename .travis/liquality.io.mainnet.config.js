b3869fc5808ef31319253d07544b864bbbf6dc59
monokh <mnokhb@gmail.com>
2019-02-01 12:00:11 +0000

    testnet and mainnet configs + deploy

--- .travis/liquality.io.mainnet.config.js
+++ .travis/liquality.io.mainnet.config.js
@@ -1,9 +1,9 @@
 export default {
   eth: {
     rpc: {
-      url: 'https://rinkeby.infura.io/pjvC14Kn4cv47X3U8qLq'
+      url: 'https://mainnet.infura.io/pjvC14Kn4cv47X3U8qLq'
     },
-    network: 'rinkeby'
+    network: 'mainnet'
   },
   btc: {
     rpc: {
@@ -14,7 +14,6 @@ export default {
     feeNumberOfBlocks: 1,
     network: 'bitcoin'
   },
-  debug: true,
   injectScript: `
   // GA
   (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -48,34 +47,10 @@ export default {
     r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
     a.appendChild(r);
   })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-
-  // CRISP
-  window.$crisp=[];
-  window.CRISP_WEBSITE_ID="aebdc8b6-79e1-4afd-83d8-70a9143bd88b";
-  (function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
-
-  // TWITTER
-  window.twttr = (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0],
-    t = window.twttr || {};
-    if (d.getElementById(id)) return t;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://platform.twitter.com/widgets.js";
-    fjs.parentNode.insertBefore(js, fjs);
-
-    t._e = [];
-    t.ready = function(f) {
-      t._e.push(f);
-    };
-
-    return t;
-  }(document, "script", "twitter-wjs"));
   `,
   injectFooter: `<p style="text-align: center;">
   <a href="https://liquality.io/terms-of-use" target="_blank">Terms of Use</a>
   &nbsp;|&nbsp;
   <a href="https://liquality.io/privacy-policy" target="_blank">Privacy Policy</a>
-  </p>`,
-  twitterButton: `?text=${encodeURIComponent('I just swapped mainnet $BTC and testnet $ETH without middlemen, fees, or counterparty risk. Thank you!')}&url=${encodeURIComponent('https://liquality.io/')}&hashtags=bitcoin,atomicswap,liquality,ethereum,atomiccrosschainswap&via=liquality_io`
+  </p>`
 }
