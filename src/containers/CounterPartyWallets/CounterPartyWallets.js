4f222f9b168d277c380332daf5d61b3bd4512515
monokh <mnokhb@gmail.com>
2019-03-06 00:20:31 +0800

    Switch around counter party wallets

--- src/containers/CounterPartyWallets/CounterPartyWallets.js
+++ src/containers/CounterPartyWallets/CounterPartyWallets.js
@@ -11,19 +11,19 @@ class CounterPartyWallets extends Component {
     return <div className='CounterPartyWallets'>
       <div className='row'>
         <div className='col'>
-          <h5 className='CounterPartyWallets_heading'>Receive From</h5>
-          <AddressInput currency={assetB.currency}
-            value={counterParty[assetB.currency].address}
-            onChange={(newValue, valid) => this.props.onCounterPartyAddressChange(assetB.currency, newValue, valid)}
-            tabIndex={4}
-          />
-        </div>
-        <div className='col'>
           <h5 className='CounterPartyWallets_heading'>Send To</h5>
           <AddressInput currency={assetA.currency}
             value={counterParty[assetA.currency].address}
             onChange={(newValue, valid) => this.props.onCounterPartyAddressChange(assetA.currency, newValue, valid)}
             tabIndex={5}
+          />
+        </div>
+        <div className='col'>
+          <h5 className='CounterPartyWallets_heading'>Receive From</h5>
+          <AddressInput currency={assetB.currency}
+            value={counterParty[assetB.currency].address}
+            onChange={(newValue, valid) => this.props.onCounterPartyAddressChange(assetB.currency, newValue, valid)}
+            tabIndex={4}
           />
         </div>
       </div>
