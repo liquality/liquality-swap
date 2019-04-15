18ca536bc56797574ce723418464027b192276f5
monokh <mnokhb@gmail.com>
2019-04-15 16:00:30 +0100

    Add hotjar to mainnet config

--- .travis/liquality.io.mainnet.config.js
+++ .travis/liquality.io.mainnet.config.js
@@ -91,10 +91,22 @@ export default {
     })(window,document,window['_fs_namespace'],'script','user');
   }
 
+  function addHotJar () {
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1260353,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+  }
+
   function addAnalytics () {
     addGA();
     addSentry();
     addFullStory();
+    addHotJar();
   }
 
   if (window.localStorage.getItem('enableAnalytics') == 'true') {
