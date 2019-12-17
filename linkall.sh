bf006fdbb8edc096668deea7e33f37018dd1d110
monokh <mnokhb@gmail.com>
2019-12-17 12:33:09 +0000

    Delete linkall.sh

--- linkall.sh
+++
@@ -1,7 +0,0 @@
-npm link @liquality/bitcoin-rpc-provider
-npm link @liquality/bitcoin-esplora-api-provider
-npm link @liquality/bitcoin-networks
-npm link @liquality/bitcoin-node-wallet-provider
-npm link @liquality/bitcoin-swap-provider
-npm link @liquality/bitcoin-utils
-npm link @liquality/bitcoin-ledger-provider
