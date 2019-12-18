9e2e32e2fc7d5ca7bfb513bfa3d639e39bf662d8
monokh <mnokhb@gmail.com>
2019-12-18 15:23:59 +0000

    Swap Integration Test: complete simple flow

--- src/test/integration/swap.test.js
+++ src/test/integration/swap.test.js
@@ -2,12 +2,18 @@
 import puppeteer from 'puppeteer'
 
 let partyA = {}
+let partyB = {}
 
 jest.setTimeout(300000)
 
 beforeEach(async () => {
   partyA.browser = await puppeteer.launch({ignoreHTTPSErrors: true})
   partyA.page = await partyA.browser.newPage()
+
+  // allow clipboard read
+  const context = partyA.browser.defaultBrowserContext()
+  context.clearPermissionOverrides()
+  context.overridePermissions('https://localhost:3000', ['clipboard-read'])
   await partyA.page.goto('https://localhost:3000')
 })
 
@@ -15,18 +21,48 @@ afterEach(async () => {
   await partyA.browser.close()
 })
 
+async function connectNodeWallet (page) {
+  await page.click('.WalletChoose_WalletsContainer div:nth-child(3)')
+  await page.click('.WalletConnectModal .btn-primary')
+  await page.waitForSelector('.WalletConnected', { timeout: 10000 })
+  await page.click('.WalletConnectModal .btn-primary')
+}
+
+async function claim (page) {
+  await page.waitForSelector('.SwapRedemption', { timeout: 20000 })
+  await page.click('.btn-primary')
+  await page.waitForSelector('.SwapCompleted')
+}
+
+async function partyAPath () {
+  await partyA.page.click('.btn-primary')
+  await partyA.page.waitForSelector('.Waiting')
+  await claim(partyA.page)
+}
+
+async function partyBPath (counterPartyLink) {
+  partyB.browser = await puppeteer.launch({ignoreHTTPSErrors: true})
+  partyB.page = await partyB.browser.newPage()
+  await partyB.page.goto(counterPartyLink)
+  await partyB.page.click('.WalletPanel_left .btn-primary')
+  await connectNodeWallet(partyB.page)
+  await partyB.page.click('.WalletPanel_right .btn-primary')
+  await connectNodeWallet(partyB.page)
+
+  // Confirm
+  await partyB.page.click('.btn-primary')
+  await partyB.page.waitForSelector('.BackupLinkCard')
+  await partyB.page.click('.btn-primary')
+  await partyB.page.waitForSelector('.Waiting')
+
+  await claim(partyB.page)
+}
+
 it('swap', async () => {
   // TODO: split into specific functions
   await partyA.page.click('.AssetSelection_bottom button')
-  await partyA.page.click('.WalletChoose_WalletsContainer div:nth-child(3)')
-  await partyA.page.click('.btn-primary')
-  await partyA.page.waitForSelector('.WalletConnected', { timeout: 20000 })
-  await partyA.page.click('.btn-primary')
-
-  await partyA.page.click('.WalletChoose_WalletsContainer div:nth-child(3)')
-  await partyA.page.click('.btn-primary')
-  await partyA.page.waitForSelector('.WalletConnected')
-  await partyA.page.click('.btn-primary')
+  await connectNodeWallet(partyA.page)
+  await connectNodeWallet(partyA.page)
 
   await partyA.page.type('.CurrencyInputs_left .CurrencyInput_input', '0.01')
   await partyA.page.type('.Rate_input', '1')
@@ -40,7 +76,7 @@ it('swap', async () => {
   const wantAddressElement = await partyA.page.$('.WalletPanel_right .WalletDisplay_address')
   const wantAddress = await partyA.page.evaluate(el => el.textContent, wantAddressElement)
 
-  await partyA.page.type('.CounterPartyWallet_send .AddressInput_input', haveAddress.replace('0x', '')) // TODO: fix this?
+  await partyA.page.type('.CounterPartyWallet_send .AddressInput_input', haveAddress)
   await partyA.page.type('.CounterPartyWallet_receive .AddressInput_input', wantAddress)
 
   // Initiate
@@ -50,7 +86,7 @@ it('swap', async () => {
   await partyA.page.click('.btn-primary')
 
   await partyA.page.waitForSelector('.CounterPartyLinkCard')
-  await partyA.page.click('.btn-primary')
-
-  await partyA.page.waitForSelector('.Waiting')
+  await partyA.page.click('.btn-secondary')
+  const counterPartyLink = await partyA.page.evaluate(() => navigator.clipboard.readText())
+  await Promise.all([partyAPath(), partyBPath(counterPartyLink)])
 })
