6e001ed69b3db5dee01f4e248920371954b4ca90
monokh <mnokhb@gmail.com>
2020-02-04 09:51:20 +0000

    Override dai icon

--- src/utils/assets.js
+++ src/utils/assets.js
@@ -1,6 +1,8 @@
 function getIcon (code) {
   try {
-    return require(`../../node_modules/cryptocurrency-icons/svg/color/${code.toLowerCase()}.svg`)
+    const codeLowerCase = code.toLowerCase()
+    if (codeLowerCase === 'dai') return require(`../../src/icons/${codeLowerCase}.svg`)
+    else return require(`../../node_modules/cryptocurrency-icons/svg/color/${codeLowerCase}.svg`)
   } catch (e) {
     return require('../icons/blank-asset.svg')
   }
