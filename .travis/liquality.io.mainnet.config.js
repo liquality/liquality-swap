e11f2f4d16b0a13daedf36c37d5b65ba12c68d09
monokh <mnokhb@gmail.com>
2020-03-13 20:12:47 +0000

    .io mainnet config - USDC

--- .travis/liquality.io.mainnet.config.js
+++ .travis/liquality.io.mainnet.config.js
@@ -35,6 +35,19 @@ export default {
       contractAddress: '0x6b175474e89094c44da98b954eedeac495271d0f',
       network: 'mainnet',
       explorerPath: 'https://etherscan.io/tx/0x'
+    },
+    usdc: {
+      type: 'erc20',
+      rpc: {
+        url: 'https://mainnet.infura.io/v3/3bbb5ebeb45e4b2b9a35261f272fb611'
+      },
+      api: {
+        type: 'scraper',
+        url: 'https://liquality.io/eth-mainnet-api'
+      },
+      contractAddress: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+      network: 'mainnet',
+      explorerPath: 'https://etherscan.io/tx/0x'
     }
   },
   hostName: 'Liquality',
