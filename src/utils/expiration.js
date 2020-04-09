6c479463a03e079f4846ff24ab36df663fcfa421
monokh <mnokhb@gmail.com>
2020-04-09 10:59:32 +0100

    Change swap expiration to 3h-6h

--- src/utils/expiration.js
+++ src/utils/expiration.js
@@ -1,8 +1,8 @@
 import moment from 'moment'
 
 const expirationDurations = {
-  a: moment.duration(12, 'h'),
-  b: moment.duration(6, 'h')
+  a: moment.duration(6, 'h'),
+  b: moment.duration(3, 'h')
 }
 
 function getExpirationForParty (expiration, party, isPartyB) {
