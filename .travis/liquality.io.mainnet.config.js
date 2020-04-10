21f21355da29e6d5f73edd5ec8fc38228dfa1a33
monokh <mnokhb@gmail.com>
2020-04-10 20:59:54 +0100

    .io configs - add GTM

--- .travis/liquality.io.mainnet.config.js
+++ .travis/liquality.io.mainnet.config.js
@@ -116,6 +116,14 @@ export default {
     };
   }
 
+  function addGTM () {
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-52D86Z5');
+  }
+
   function addFullStory () {
     window['_fs_debug'] = false;
     window['_fs_host'] = 'fullstory.com';
@@ -146,6 +154,7 @@ export default {
   }
 
   function addAnalytics () {
+    addGTM();
     addGA();
     addIntercom();
     addSentry();
@@ -229,5 +238,9 @@ export default {
     </div>
   </div>
   <img src onerror="termsLoaded()">
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-52D86Z5"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
   `
 }
