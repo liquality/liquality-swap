2f5780ec176586a89c2bf3e593cc1fc4847fe73b
gr0kchain <gr0kchain@bitcoindev.network>
2020-05-03 00:13:43 +0200

    Removed btc agent

--- .travis/liquality.io.mainnet.config.js
+++ .travis/liquality.io.mainnet.config.js
@@ -52,7 +52,7 @@ export default {
   },
   hostName: 'Liquality',
   hostIcon: 'https://raw.githubusercontent.com/liquality/chainabstractionlayer/master/liquality-logo.png',
-  agents: ['https://liquality.io/swap/agent', 'https://liquality.io/swap/btc'],
+  agents: ['https://liquality.io/swap/agent'],
   injectScript: `
   function addSentry () {
     (function loadScript(src, callback) {
