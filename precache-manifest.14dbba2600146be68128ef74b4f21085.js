6ea8a970b0f6ab72ded3b3f1551069baf30a6c3f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-12 09:57:55 +0000

    Deploy liquality/liquality-swap to github.com/liquality/liquality-swap.git:gh-pages

---
+++ precache-manifest.14dbba2600146be68128ef74b4f21085.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "87bd3959b1a819c7013c2d5857bf80c0",
+    "url": "./static/media/spinner.87bd3959.svg"
+  },
+  {
+    "revision": "8fcd70aa6b3e77e2b524",
+    "url": "./static/css/main.5d94099c.chunk.css"
+  },
+  {
+    "revision": "5c5937b0fd48f019b850",
+    "url": "./static/js/1.5c5937b0.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "db5d5e55c30b00d8726047ab345fe137",
+    "url": "./static/media/tick.db5d5e55.svg"
+  },
+  {
+    "revision": "b54df648bd2033331b5071478b08ec97",
+    "url": "./static/media/device.b54df648.svg"
+  },
+  {
+    "revision": "923d9605ac6e8564d779b90b130fb2cd",
+    "url": "./static/media/copy.923d9605.svg"
+  },
+  {
+    "revision": "575bbbb45fb6932b9728719a94415714",
+    "url": "./static/media/clock.575bbbb4.svg"
+  },
+  {
+    "revision": "5ba281d089f2ca87b031f7f239efe066",
+    "url": "./static/media/close.5ba281d0.svg"
+  },
+  {
+    "revision": "602cfc4a599f5462b0fd57a54ffab865",
+    "url": "./static/media/icon.602cfc4a.svg"
+  },
+  {
+    "revision": "e0ea0e03020bbe9fd9a963408c138640",
+    "url": "./static/media/icon.e0ea0e03.svg"
+  },
+  {
+    "revision": "346d077f5f12f96c09c318a7d95d9e8f",
+    "url": "./static/media/handshake.346d077f.png"
+  },
+  {
+    "revision": "1755dc6e11b57d4d7b7e1aa113f1d848",
+    "url": "./static/media/wallet-icon.1755dc6e.svg"
+  },
+  {
+    "revision": "78698de6d23388ca408994aa08d4e821",
+    "url": "./static/media/icon.78698de6.svg"
+  },
+  {
+    "revision": "949106f1f3ca8f9e937cb316f5a75d66",
+    "url": "./static/media/icon.949106f1.svg"
+  },
+  {
+    "revision": "66c0cc23339c809bd27e53fceebd2766",
+    "url": "./static/media/error.66c0cc23.svg"
+  },
+  {
+    "revision": "ae3de156134436c341b9b6d981a74905",
+    "url": "./static/media/info.ae3de156.svg"
+  },
+  {
+    "revision": "288f52b800327c5618706c6354602cc5",
+    "url": "./static/media/lock.288f52b8.svg"
+  },
+  {
+    "revision": "8fcd70aa6b3e77e2b524",
+    "url": "./static/js/main.8fcd70aa.chunk.js"
+  },
+  {
+    "revision": "0151f9986d6ff043178d37208a73298c",
+    "url": "./static/media/switch.0151f998.svg"
+  },
+  {
+    "revision": "3e19fc7bc002823cde78440713e3aeaf",
+    "url": "./static/media/blank-asset.3e19fc7b.svg"
+  },
+  {
+    "revision": "a596700eb6203303b1aabd3f37fd4715",
+    "url": "./static/media/spinner.a596700e.svg"
+  },
+  {
+    "revision": "c9c4070beb0c69823bb1f612959e22dd",
+    "url": "./static/media/assets-bg.c9c4070b.svg"
+  },
+  {
+    "revision": "91b58e9eb410cb4b37b381a4fcd61759",
+    "url": "./static/media/tick.91b58e9e.svg"
+  },
+  {
+    "revision": "9c76fef58cf4c5fca97dfe6b019a90a1",
+    "url": "./static/media/error.9c76fef5.svg"
+  },
+  {
+    "revision": "eb6deb9fe392d186281a7b896689ae61",
+    "url": "./static/media/warning.eb6deb9f.png"
+  },
+  {
+    "revision": "3c2c333802ceffc62537a6971ea56d0a",
+    "url": "./static/media/logo-text.3c2c3338.png"
+  },
+  {
+    "revision": "e73cb1b222d746763a3d57ab92c94621",
+    "url": "./static/media/btc.e73cb1b2.svg"
+  },
+  {
+    "revision": "b1e99a4478a65287beb1c4ccba59c00c",
+    "url": "./static/media/dai.b1e99a44.svg"
+  },
+  {
+    "revision": "18708d3e3f54116e2ea639fdd56dd6c5",
+    "url": "./static/media/eth.18708d3e.svg"
+  },
+  {
+    "revision": "eb5e5cf984ba2f9735ae557e51509174",
+    "url": "./static/media/rbtc.eb5e5cf9.svg"
+  },
+  {
+    "revision": "0eb4419f9cf6c297a1b9aa547f735b6b",
+    "url": "./static/media/uni.0eb4419f.svg"
+  },
+  {
+    "revision": "a2b9c7479efa3c4dcf2494f333eac02d",
+    "url": "./static/media/usdc.a2b9c747.svg"
+  },
+  {
+    "revision": "0bab137e0d87892a339049b5ddbd40d6",
+    "url": "./static/media/usdt.0bab137e.svg"
+  },
+  {
+    "revision": "eafc06b49cff4d2f8eb4f922ebfe2e56",
+    "url": "./static/media/wbtc.eafc06b4.svg"
+  },
+  {
+    "revision": "d4c055194d2ba86d26b9fa3140ae06b6",
+    "url": "./index.html"
+  }
+];
