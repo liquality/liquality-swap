1866f37c1c5ce34bdf78f6827fa34e9ca04e712b
unknown <sachscarl@gmail.com>
2021-04-05 12:56:41 -0400

    dropdown bar added and dynamic with assetUtils.  Working on switching currencies on click

--- src/components/CurrencyInput/CurrencyInput.js
+++ src/components/CurrencyInput/CurrencyInput.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import BigNumber from 'bignumber.js'
 import DropDownTick from '../../icons/dropdownTick.svg'
+import * as assetUtils from '../../utils/assets'
+import config from '../../config'
 
+import Dropdown from 'react-bootstrap/Dropdown'
+import FormControl from 'react-bootstrap/FormControl'
 
 import cryptoassets from '@liquality/cryptoassets'
 import './CurrencyInput.css'
@@ -12,7 +16,8 @@ class CurrencyInput extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      valueString: props.value.toFixed()
+      valueString: props.value.toFixed(),
+      show: false
     }
   }
 
@@ -71,12 +76,43 @@ class CurrencyInput extends Component {
 
   render () {
     const asset = cryptoassets[this.props.currency]
+    const userDefinedAssets = Object.keys(config.assets)
+    const displayedAssets = Object.entries(cryptoassets)
+      .filter(([id]) => userDefinedAssets.includes(id))
+
+    //DROPDOWN
+    const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+      <a
+        href=""
+        ref={ref}
+        onClick={(e) => {
+          e.preventDefault();
+          onClick(e);
+        }}
+      >
+        {children}
+        &#x25bc;
+      </a>
+    ));
+
     return <div className='CurrencyInput'>
       { this.props.fiatRate && !this.props.value.isNaN() && <div className='CurrencyInput_price'>
         ${ this.getFiatValue() } USD
       </div> }
       <div className="CurrencyInput_inputBigWrap">
-      <h3 className='CurrencyInput_heading'>{asset.code}<img src={DropDownTick} alt='dropdown toggler' className="ml-2" /></h3>
+      <div className="CurrencyInput_DropdownWrap">
+      <Dropdown>
+        <Dropdown.Toggle as={CustomToggle} id="dropdown-basic" className="CurrencyInput_toggler">
+          <img src={assetUtils.getIcon(asset.code)} alt='asset icon' /> <h1>{asset.code}</h1>
+        </Dropdown.Toggle>
+      
+        <Dropdown.Menu>
+          {displayedAssets.map(([id, currency]) => 
+            <Dropdown.Item href="#" key={id}><img src={assetUtils.getIcon(currency.code)} alt="currency icon/logo" style={{height: "25px", width: "25px", marginRight: "5%"}} /><strong>{currency.code}</strong></Dropdown.Item>
+          )}
+        </Dropdown.Menu>
+      </Dropdown>
+      </div>
       </div>
       <div className={classNames('CurrencyInput_inputWrapper', { 'disabled': this.props.disabled })}>
         <input type='number' min='0' readOnly={this.props.disabled} value={this.restrictNumber(this.state.valueString)}
